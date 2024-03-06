088b98ce086725d4d8c89f016d26887fb72820c0
ryan siddoway <ryansiddoway@icloud.com>
2024-03-06 16:32:45 -0700

    "upload files"

---
+++ src/app/world/world.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-world',
+  templateUrl: './world.component.html',
+  styleUrls: ['./world.component.css'],
+  standalone: true
+})
+export class WorldComponent implements OnInit{
+
+  constructor (private apiService:ApiService) {}
+
+  ngOnInit(): void {
+    let svgPaths = document.querySelectorAll<SVGPathElement>('path');
+    svgPaths.forEach(svgCountry => {
+
+      svgCountry.addEventListener('mouseover', event => {
+        const path = event.target as SVGPathElement;
+        if (path) {
+          path.style.fill = 'red';
+          this.apiService.getCountryData(svgCountry)
+        }
+      });
+    
+      svgCountry.addEventListener('mouseleave', event => {
+        const path = event.target as SVGPathElement;
+        if (path) {
+          path.style.fill = '';
+        }
+      });
+    });
+  }
+
+  
+}
